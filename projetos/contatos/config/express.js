e8001489f8981eef2d53d91f8449f5c05329bfbc
jose alves sousa neto <a.sousajose@gmail.com>
2016-06-07 00:19:20 -0400

    projeto git

---
+++ projetos/contatos/config/express.js
@@ -0,0 +1,17 @@
+//config/express.js
+var express = require('express'),
+    load = require('express-load');
+
+module.exports = function () {
+    var app = express();
+    app.set('port', 3000);
+
+    app.use(express.static('./public'));
+
+    load('models')
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+    return app;
+}
