86cd827c43fcaf57254d0e6764d3c7a00912ee4c
jose alves sousa neto <a.sousajose@gmail.com>
2016-07-15 22:00:00 -0400

    Angular
    Aula sobre SPA e rotas do angular

--- projetos/contatos/public/js/main.js
+++ projetos/contatos/public/js/main.js
@@ -1,1 +1,2 @@
-angular.module('contatos', []);
+angular.module('contatos', ['ngRoute'])
+;
---
+++ projetos/contatos/public/js/routes/routes.js
@@ -0,0 +1,11 @@
+angular.module('contatos')
+  .config(function ($routeProvider) {
+
+    $routeProvider.when('/contatos', {
+      templateUrl: 'partials/contatos.html',
+      controller: 'contatosController'
+    });
+
+    $routeProvider.otherwise(
+        {redirectTo: '/contatos'});
+  });
